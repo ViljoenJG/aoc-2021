316182fdd9eeafa62c84a91237d34d46e2d7e510
Kobus Viljoen <kobusv@icetech.io>
2021-12-07 09:24:21 +0200

    Added more util parse functions

--- lib/advent_of_code/utils.ex
+++ lib/advent_of_code/utils.ex
@@ -4,9 +4,20 @@ defmodule AdventOfCode.Utils do
     |> String.split("\n", trim: true)
   end
 
+  def parse(input, sep, options \\ []) do
+    input
+    |> String.split(sep, options)
+  end
+
   def parse_as_int(input) do
     input
     |> parse
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def parse_as_int(input, sep, options \\ []) do
+    input
+    |> parse(sep, options)
     |> Enum.map(&String.to_integer/1)
   end
 end
