3b1c9eedd6668f784f32b6fbe67c01ec2cada6ee
Kobus Viljoen <kobusv@icetech.io>
2021-12-07 09:24:49 +0200

    Added day 7 solutions.

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Day07 do
+  alias AdventOfCode.Utils
+
   def part1(args) do
+    args
+    |> String.trim()
+    |> Utils.parse_as_int(",", trim: true)
+    |> then(fn x ->
+      min = Enum.min(x)
+      max = Enum.max(x)
+
+      min..max
+      |> Enum.map(fn y ->
+        x
+        |> Enum.map(&abs(y - &1))
+        |> Enum.sum()
+      end)
+    end)
+    |> Enum.min()
   end
 
   def part2(args) do
+    f = fn n -> n * (n + 1) / 2 end
+
+    args
+    |> String.trim()
+    |> Utils.parse_as_int(",", trim: true)
+    |> then(fn x ->
+      min = Enum.min(x)
+      max = Enum.max(x)
+
+      min..max
+      |> Enum.map(fn y ->
+        x
+        |> Enum.map(&f.(abs(y - &1)))
+        |> Enum.sum()
+      end)
+    end)
+    |> Enum.min()
   end
 end
